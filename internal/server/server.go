b8d2a543422aff855f72cb55ba503f9583f28cc1
leanghok <leanghokoeng5@gmail.com>
2025-07-17 16:46:34 +0700

    add minimal css

--- internal/server/server.go
+++ internal/server/server.go
@@ -8,11 +8,13 @@ import (
 
 func New() *http.Server {
 	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("static"))
 
 	mux.HandleFunc("/", handlers.HandleRoot)
 	mux.HandleFunc("GET /pastes", handlers.GetPastes)
 	mux.HandleFunc("GET /pastes/{id}", handlers.GetPaste)
 	mux.HandleFunc("POST /pastes", handlers.CreatePaste)
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	return &http.Server{
 		Addr:    ":8080",
